3c16b355ee5b3e71fbf1efb78c24353aed32cef7
Vibhishan Ranga <vibhishanranga@Vibhishans-MacBook-Pro-3.local>
2024-09-22 21:13:27 +0100

    code for bot class
    Signed-off-by: Vibhishan Ranga <vibhishanranga@Vibhishans-MacBook-Pro-3.local>

--- src/main/java/me/vibhishanranga/tictactoe/models/Bot.java
+++ src/main/java/me/vibhishanranga/tictactoe/models/Bot.java
@@ -1,4 +1,22 @@
 package me.vibhishanranga.tictactoe.models;
 
-public class Bot {
+import me.vibhishanranga.tictactoe.factories.botPlayingStrategyFactory.BotPlayingStrategyFactory;
+import me.vibhishanranga.tictactoe.strategies.botPlayingStrategies.BotPlayingStrategy;
+
+public class Bot extends Player {
+    private BotPlayingStrategy botPlayingStrategy;
+    private BotDifficultyLevel botDifficultyLevel;
+
+    public Bot(Symbol symbol, BotDifficultyLevel botDifficultyLevel) {
+        super(PlayerType.BOT, symbol);
+        this.botDifficultyLevel = botDifficultyLevel;
+        this.botPlayingStrategy = new BotPlayingStrategyFactory().createBotPlayingStrategyForDifficultyLevel(
+                botDifficultyLevel
+        );
+    }
+
+    @Override
+    public Move makeMove(Board board) {
+        return this.botPlayingStrategy.makeNextMove(board, this);
+    }
 }
